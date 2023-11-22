19db8a45cd6d154504eed49645a23dcc0e903873
dong-kj <tjfansghk@gmail.com>
2023-11-22 13:09:39 +0900

    start Pj
    프로젝트 기초

---
+++ init.js
@@ -0,0 +1,7 @@
+import app from "./server";
+
+const PORT =3000;
+const handleListening = ()=>{
+  console.log(`Server Listening on http://localhost:${PORT}`);
+}
+app.listen(PORT, handleListening);
